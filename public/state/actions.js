d9a0d068e0dd5cf55d6001837f349d58bf9cda2e
Rok Zigon <roky.zz@gmail.com>
2018-07-13 02:30:10 +0200

    initial actions

---
+++ public/state/actions.js
@@ -0,0 +1,11 @@
+"use strict";
+
+export default {
+	// FETCHING DATA
+	FETCHING_DATA_START: "fetching_start",
+	FETCHING_DATA_FAIL: "fetching_fail",
+	FETCHING_DATA_SUCCESS: "fetching_success",
+	// SETTINGS
+	OPEN_SETTINGS: "open_settings",
+	CHANGE_SETTINGS: "change_setings"
+};
