961edb15c81dfc41d88157ae3b16b7400ba779ba
Rok Zigon <roky.zz@gmail.com>
2018-07-15 22:32:51 +0200

    added select, prepare & fetch details action

--- public/state/actions.js
+++ public/state/actions.js
@@ -4,7 +4,10 @@ export default {
 	// FETCHING DATA
 	FETCHING_DATA_START: "fetching_start",
 	FETCHING_DATA_FAIL: "fetching_fail",
-	FETCHING_DATA_SUCCESS: "fetching_success",
+	FETCHING_LIST_SUCCESS: "fetching_list_success",
+	FETCHING_DETAILS_SUCCESS: "fetching_details_success",
+	SET_SELECTED_ID: "set_selected_id",
+	PREPARE_UPDATE: "prepare_update",
 	// SETTINGS
 	OPEN_SETTINGS: "open_settings",
 	CHANGE_SETTINGS: "change_setings"
