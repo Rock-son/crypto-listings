82224dde6e3db9ee629c14fa5f4580a151fe2be5
Rok Zigon <roky.zz@gmail.com>
2018-07-15 23:03:20 +0200

    changed static express logic+added serve-favicon (due to new express v.)

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const http = require("http");
 const path = require("path");
 const fs = require("fs");
 const bodyParser = require("body-parser");
+const serveStatic = require('serve-static');
+const favicon = require('serve-favicon');
 // EXPRESS LIMITER
 const RateLimiter = require("express-rate-limit");
 // SECURITY
@@ -35,8 +37,9 @@ const limiter = new RateLimiter({
 app.set("views", path.join(__dirname, "dist"));
 app.set("view engine", "pug");
 // ROUTES
-app.use(express.static(path.join(__dirname, "dist")));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(serveStatic(path.join(__dirname, "dist")));
+app.use(serveStatic(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, 'public/assets/', 'favicon.ico')));
 // BODY PARSERS
 app.use(bodyParser.json({ type: "application/json" }));
 app.use(bodyParser.json({ type: ["json", "application/csp-report"] }));
@@ -57,8 +60,8 @@ app.use(limiter);
 router(app);
 
 // PUT ALL ROUTES ABOVE THIS LINE OF CODE!
-	// * NEEDED FOR REACT ROUTER HISTORY LIB
-	app.get("*", (req, res) => res.sendFile(path.join(__dirname, "dist", "index.html")));
+// * NEEDED FOR REACT ROUTER HISTORY LIB
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "dist", "index.html")));
 
 
 // SERVER
